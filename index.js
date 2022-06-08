c3732c8dd540f0a48d15849bd0dafa1062ff5b30
Kurt Baux <ktripodibaux@gmail.com>
2022-06-08 09:44:39 -0400

    commit tree df872165c4f06193bd37570404d54e657f346631

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+    return cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+    return cats.unshift(cat);
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function appendCat(cat) {
+    const newArr = [...cats];
+    newArr.push(cat);
+    return newArr;
+}
+
+function prependCat(cat) {
+    const newArr = [...cats];
+    newArr.unshift(cat);
+    return newArr;
+}
+
+function removeLastCat() {
+    const newArr = [...cats];
+    newArr.pop();
+    return newArr;
+}
+
+function removeFirstCat() {
+    const newArr = [...cats];
+    newArr.shift();
+    return newArr;
+}
